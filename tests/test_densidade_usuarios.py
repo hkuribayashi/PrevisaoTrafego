3c3b24e6204021a911a7eb2a08f2cbe9044322a1
Hugo Kuribayashi <hkuribayashi@gmail.com>
2020-04-13 11:43:48 -0300

    Alteração nos parâmetros de geração da curva de gompertz

--- tests/test_densidade_usuarios.py
+++ tests/test_densidade_usuarios.py
@@ -5,30 +5,27 @@ from library.util.Util import get_gompertz
 
 # Teste para geracao de taxa de usuários de Internet
 a = 0.95
+b = 5
 tx = 10
 time = np.arange(tx)
 
-b = 5
+
 c = 0.15
 h = get_gompertz(a, b, c, tx)
 
-b = 5
 c = 0.25
 h1 = get_gompertz(a, b, c, tx)
 
-b = 5
 c = 0.35
 h2 = get_gompertz(a, b, c, tx)
 
-b = 5
 c = 0.45
 h3 = get_gompertz(a, b, c, tx)
 
-
-plt.plot(h, 'g-*', label='b=5')
-plt.plot(h1, 'b--', label='b=10')
-plt.plot(h2, 'r-.', label='b=15')
-plt.plot(h3, '-o', label='b=25')
+plt.plot(h, 'g-*', label='c=0.15')
+plt.plot(h1, 'b--', label='c=0.25')
+plt.plot(h2, 'r-.', label='c=0.35')
+plt.plot(h3, '-o', label='c=0.45')
 plt.xlabel('Período de Análise (Anos)')
 plt.ylabel('Taxa de Usuários de Internet')
 plt.legend(loc='upper left')
